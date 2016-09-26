b960673f38e35c08242b4af146a6a281cb2d0a39
Alex Craven <w.alex.craven@gmail.com>
2016-09-26 12:24:24 -0500

    added post msg to mongodb

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { json } = require('body-parser');
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -8,6 +9,7 @@ const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meanch
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('client'))
+app.use(json())
 
 app.get('/api/title', (req, res) =>
   res.json({ title: 'MEAN Chat' })
@@ -24,6 +26,14 @@ app.get('/api/messages', (req, res, err) =>
     .then(messages => res.json({ messages }))
     .catch(err)
 )
+
+app.post('/api/messages', (req, res, err) => {
+  const msg = req.body
+  Message
+    .create(msg)
+    .then(msg => res.json(msg))
+    .catch(err)
+})
 
 mongoose.promise = Promise
 mongoose.connect(MONGODB_URL, () =>
